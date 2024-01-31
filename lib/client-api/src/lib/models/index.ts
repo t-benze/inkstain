66b3ece7428fcb6ef82ee6161c02064ac386546e
Benze Tang <tangbenze@gmail.com>
2024-01-31 17:02:40 +0800

    feat: support upload document and list folder

--- lib/client-api/src/lib/models/index.ts
+++ lib/client-api/src/lib/models/index.ts
@@ -1,6 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './DocumentsSpaceNameListGet200ResponseInner';
 export * from './PlatformDirectoriesPathGet200ResponseInner';
 export * from './PlatformGet200Response';
 export * from './Space';
 export * from './SpacesNamePutRequest';
+export * from './SpacesPostRequest';
